5883c8c6c6a8e8d7d67dfccaca6b26d1d11c162c
Alyssa Reese <alyssa.reese@gmail.com>
2013-05-05 23:35:43 -0700

    getting ready for heroku

--- bin/buzzwordbot.js
+++ bin/buzzwordbot.js
@@ -10,14 +10,18 @@ var argv = optimist
 		alias: 'choose',
 		describe: 'Choose a story to publish'
 	})
+	.options('d', {
+		alias: 'daemon',
+		describe: 'run as daemon process'
+	})
 	.usage("Usage:\n\
 		\tbuzzwordbot --choose\tchoose a story to publish from our set of RSS feeds.\n\
 		")
 	.argv;
 
-if (argv.choose) {
-	console.log('grabbing stories from RSS feeds...')
-	new RSSReader().readAllFeeds(function(err, stories) {
+function publishStory(rssReader, storyChoose, tweeter) {
+	rssReader.readAllFeeds(function(err,stories) {
+
 		if(err) {
 			console.log('an error occured', err);
 			return;
@@ -26,16 +30,34 @@ if (argv.choose) {
 		console.log('found ', stories.length, 'articles.')
 
 		console.log('finding candidate stories to publish')
-		new StoryChooser().chooseStory(stories, function(story) {
+		storyChooser.chooseStory(stories, function(story) {
 			if (story) {
 			console.log('found story', story.title);
-			new Tweeter().tweetArticle(story);
+			tweeter.tweetArticle(story);
 			} else {
 				console.log ('no story found.')
 			}
 		});
 
 	});
+}
+
+if (argv.choose) { // Publish a single story for testing purposes.
+  var rssReader = new RSSReader(),
+    storyChooser = new StoryChooser(),
+    tweeter = new Tweeter();
+
+  publishStory(rssReader, storyChooser, tweeter);
+
+} else if (argv.daemon) { // start a daemon that publishes stories.
+  var rssReader = new RSSReader(),
+    storyChooser = new StoryChooser(),
+    tweeter = new Tweeter();
+
+  setInterval(function() {
+    publishStory(rssReader, storyChooser, tweeter);
+  }, 300 * 1000) // tweet once every 5 minutes.
+
 } else {
 	console.log(optimist.help());
 }
